1b4bff79bf925bae5dcf5ae0d8f40c57a1933bd5
andy_yang <andy_yang@sjtu.edu.cn>
2022-12-13 23:05:48 +0800

    Update gemmStochasticGenerate.py

--- src/tvm/gemmStochasticGenerate.py
+++ src/tvm/gemmStochasticGenerate.py
@@ -8,153 +8,155 @@ from tvm import te
 from tvm.target import Target
 
 def stochastic_schedule(sch: tvm.tir.Schedule):
-
+    
     sch.work_on("mm")
     
     block_Y = sch.get_block(name="Y")
     
     C_local_block = sch.cache_write(block_Y,0,"local")
-    
-    A_load_block = sch.cache_read(block_Y,0,"local")
-    B_load_block = sch.cache_read(block_Y,1,"local")
-    
-    
     A_shared_block = sch.cache_read(block_Y,0,"shared")
     B_shared_block = sch.cache_read(block_Y,1,"shared")
     
     # padding for prime numbers work
     # sch.pad_einsum(block=block_Y,padding=[15,15,15])
     
-    
+    A_load_block = sch.cache_read(A_shared_block,0,"local")
+    B_load_block = sch.cache_read(B_shared_block,0,"local")
     A_local_block = sch.cache_read(block_Y,0,"local")
     B_local_block = sch.cache_read(block_Y,1,"local")
     
-    i, j, k = sch.get_loops(block=block_Y)    
+    i, j, k = sch.get_loops(block=block_Y)
+
+    # vbx,vtx,vtm = sch.sample_perfect_tile(loop=i,n=3,decision=[8,16,8]) 
+    # vby,vty,vtn = sch.sample_perfect_tile(loop=j,n=3,decision=[8,16,8])
+    # vbk,vtk = sch.sample_perfect_tile(loop=k,n=2,decision=[128,8])
     
-    vbx,vvx,vtx,vtm1,vtm2 = sch.sample_perfect_tile(loop=i,n=5,decision=[8,1,16,1,8]) 
-    vby,vvy,vty,vtn1,vtn2 = sch.sample_perfect_tile(loop=j,n=5,decision=[8,1,16,1,8])
-    vbk,vtk1,vtk2 = sch.sample_perfect_tile(loop=k,n=3,decision=[128,1,8])
+    vbx,vtx,vtm = sch.sample_perfect_tile(loop=i,n=3) 
+    vby,vty,vtn = sch.sample_perfect_tile(loop=j,n=3)
+    vbk,vtk = sch.sample_perfect_tile(loop=k,n=2)
+
+    ifactors = [vbx,vtx,vtm]
+    jfactors = [vby,vty,vtn]
+    kfactors = [vbk,vtk]
+
+
+    bx,tx,tm = sch.split(loop=i,factors=ifactors)
+    by,ty,tn = sch.split(loop=j,factors=jfactors)
+    bk,tk = sch.split(loop=k,factors=kfactors)
     
-    # vbx,vvx,vtx,vtm1,vtm2 = sch.sample_perfect_tile(loop=i,n=5) 
-    # vby,vvy,vty,vtn1,vtn2 = sch.sample_perfect_tile(loop=j,n=5)
-    # vbk,vtk1,vtk2 = sch.sample_perfect_tile(loop=k,n=3)
-    
-    
-    ifactors = [vbx,vvx,vtx,vtm1,vtm2]
-    jfactors = [vby,vvy,vty,vtn1,vtn2]
-    kfactors = [vbk,vtk1,vtk2]
-    
-    
-    bx,vx,tx,tm1,tm2 = sch.split(loop=i,factors=ifactors)
-    by,vy,ty,tn1,tn2 = sch.split(loop=j,factors=jfactors)
-    bk,tk1,tk2 = sch.split(loop=k,factors=kfactors)
-    
-    sch.reorder(by,bx,vy,vx,ty,tx,bk,tk1,tm1,tn1,tk2,tm2,tn2)
+    sch.reorder(by,bx,ty,tx,bk,tk,tm,tn)
     sch.bind(by,"blockIdx.y")
     sch.bind(bx,"blockIdx.x")
-    sch.bind(vy,"vthread.y")
-    sch.bind(vx,"vthread.x")
     sch.bind(ty,"threadIdx.y")
     sch.bind(tx,"threadIdx.x")
     
-    sch.reverse_compute_at(C_local_block,tx,preserve_unit_loops=True)
-    sch.compute_at(A_local_block,tk2,preserve_unit_loops=True)
-    sch.compute_at(B_local_block,tk2,preserve_unit_loops=True)
+    
+    sch.compute_at(A_local_block,tk,preserve_unit_loops=True)
+    sch.compute_at(B_local_block,tk,preserve_unit_loops=True)
+    sch.reverse_compute_at(block=C_local_block,loop=tx,preserve_unit_loops=True)
     sch.compute_at(A_shared_block,bk,preserve_unit_loops=True)
     sch.compute_at(B_shared_block,bk,preserve_unit_loops=True)
     sch.compute_at(A_load_block,bk,preserve_unit_loops=True)
     sch.compute_at(B_load_block,bk,preserve_unit_loops=True)
     
-    # sch.compute_at(A_local_block,tk2,preserve_unit_loops=True)
-    # vload = sch.sample_categorical(candidates=[1, 2, 3, 4], probs=[0.25, 0.25, 0.25, 0.25])
-    vload = sch.sample_categorical(candidates=[1, 2, 3, 4], probs=[0.25, 0.25, 0.25, 0.25],decision=3)
-    by, bx, vy, vx, ty, tx, bk, tk1, tm1, tn1, tk2, ax0, ax1 = sch.get_loops(A_local_block)
-    ax0 = sch.fuse(ax0,ax1)
-    ax0, ax1 = sch.split(loop=ax0,factors=[None,vload])
-    sch.unroll(ax0)
-    sch.vectorize(ax1)
+    # print(sch.mod.script())
     
-    # sch.compute_at(B_local_block,tk2,preserve_unit_loops=True)
-    # vload = sch.sample_categorical(candidates=[1, 2, 3, 4], probs=[0.25, 0.25, 0.25, 0.25])
-    vload = sch.sample_categorical(candidates=[1, 2, 3, 4], probs=[0.25, 0.25, 0.25, 0.25],decision=3)
-    by, bx, vy, vx, ty, tx, bk, tk1, tm1, tn1, tk2, ax0, ax1 = sch.get_loops(B_local_block)
+    # A_shared_block = sch.cache_read(block_Y,0,"shared")
+    
+    by, bx, ty, tx, bk, ax0, ax1 = sch.get_loops(A_shared_block)
     ax0 = sch.fuse(ax0,ax1)
-    ax0, ax1 = sch.split(loop=ax0,factors=[None,vload])
-    sch.unroll(ax0)
-    sch.vectorize(ax1)
+    ax0, ax1, ax2 = sch.split(loop=ax0, factors=[vty,vtx,None])
+    sch.bind(ax0,"threadIdx.y")
+    sch.bind(ax1,"threadIdx.x")
     
     sch.transform_layout(A_shared_block,buffer=("write",0),index_map = lambda i , j: (j,i))
     
-    # sch.compute_at(A_shared_block,bk,preserve_unit_loops=True)
-    # vload = sch.sample_categorical(candidates=[1, 2, 3, 4], probs=[0.25, 0.25, 0.25, 0.25])
-    vload = sch.sample_categorical(candidates=[1, 2, 3, 4], probs=[0.25, 0.25, 0.25, 0.25],decision=3)
-    by, bx, vy, vx, ty, tx, bk, ax0, ax1 = sch.get_loops(block=A_shared_block)
+    # B_shared_block = sch.cache_read(block_Y,1,"shared")
+    
+    # TODO decompose padding
+    by, bx, ty, tx, bk, ax0, ax1 = sch.get_loops(B_shared_block)
     ax0 = sch.fuse(ax0,ax1)
-    # ty, tx, ax0, ax1 = sch.split(loop=ax0,factors=[vty,vtx,None,vload])
-    ax0, ty, tx, ax1 = sch.split(loop=ax0,factors=[None,vty,vtx,vload])
-    # sch.unroll(ax0)
-    sch.bind(ty,"threadIdx.y")
-    sch.bind(tx,"threadIdx.x")
-    sch.vectorize(ax1)
+    ax0, ax1, ax2 = sch.split(loop=ax0, factors=[vty,vtx,None])
+    sch.bind(ax0,"threadIdx.y")
+    sch.bind(ax1,"threadIdx.x")
+    # ax0 , ax1 = sch.split(loop=ax0,factors=[tvm.tir.Mul(vtx,vty),None])
+    # sch.bind(ax0,"threadIdx.x")
     
-    # sch.compute_at(B_shared_block,bk,preserve_unit_loops=True)
-    # vload = sch.sample_categorical(candidates=[1, 2, 3, 4], probs=[0.25, 0.25, 0.25, 0.25])
-    vload = sch.sample_categorical(candidates=[1, 2, 3, 4], probs=[0.25, 0.25, 0.25, 0.25],decision=3)
-    by, bx, vy, vx, ty, tx, bk, ax0, ax1 = sch.get_loops(block=B_shared_block)
+    by, bx, ty, tx, bk, tk, tm, tn = sch.get_loops(block=block_Y)
+    
+    
+    # sch.annotate(block_or_loop=A_shared_block,ann_key="tir.manifest_shared_memory_local_stage",ann_val=1)
+    # sch.annotate(block_or_loop=B_shared_block,ann_key="tir.manifest_shared_memory_local_stage",ann_val=1)
+    # A_load_block = sch.cache_read(A_shared_block,0,"local")
+    
+    by, bx, ty, tx, bk, ax0, ax1 = sch.get_loops(A_load_block)
     ax0 = sch.fuse(ax0,ax1)
-    # ty, tx, ax0, ax1 = sch.split(loop=ax0,factors=[vty,vtx,None,vload])
-    ax0, ty, tx, ax1 = sch.split(loop=ax0,factors=[None,vty,vtx,vload])
-    # sch.unroll(ax0)
-    sch.bind(ty,"threadIdx.y")
-    sch.bind(tx,"threadIdx.x")
-    sch.vectorize(ax1)
-    
-    
-    # A_load_block = sch.cache_read(A_shared_block,0,storage_scope="local")
-    # sch.compute_at(A_load_block,bk,preserve_unit_loops=True)
-    # vload = sch.sample_categorical(candidates=[1, 2, 3, 4], probs=[0.25, 0.25, 0.25, 0.25])
-    vload = sch.sample_categorical(candidates=[1, 2, 3, 4], probs=[0.25, 0.25, 0.25, 0.25],decision=3)
-    by, bx, vy, vx, ty, tx, bk, ax0, ax1 = sch.get_loops(block=A_load_block)
-    ax0 = sch.fuse(ax0,ax1)
-    # ty, tx, ax0, ax1 = sch.split(loop=ax0,factors=[vty,vtx,None,vload])
-    ax0, ty, tx, ax1 = sch.split(loop=ax0,factors=[None,vty,vtx,vload])
-    # sch.unroll(ax0)
-    sch.bind(ty,"threadIdx.y")
-    sch.bind(tx,"threadIdx.x")
-    sch.vectorize(ax1)
-    
-    sch.transform_layout(block=B_local_block,buffer=("read",0),index_map= lambda i,j:( i , ((j//128)*128+(j%128)//8*4+(j%8)//4*64+j%4)))
-    sch.transform_layout(block=A_local_block,buffer=("read",0),index_map= lambda j,i:( j , ((i//128)*128+(i%128)//8*4+(i%8)//4*64+i%4)))
+    ax0, ax1, ax2 = sch.split(loop=ax0,factors=[vty,vtx,None])
+    sch.bind(ax0,"threadIdx.y")
+    sch.bind(ax1,"threadIdx.x")
 
+
+    # B_load_block = sch.cache_read(B_shared_block,0,"local")
     
-    # B_load_block = sch.cache_read(B_shared_block,0,storage_scope="local")
-    # sch.compute_at(B_load_block,bk,preserve_unit_loops=True)
-    # vload = sch.sample_categorical(candidates=[1, 2, 3, 4], probs=[0.25, 0.25, 0.25, 0.25])
-    vload = sch.sample_categorical(candidates=[1, 2, 3, 4], probs=[0.25, 0.25, 0.25, 0.25],decision=3)
-    by, bx, vy, vx, ty, tx, bk, ax0, ax1 = sch.get_loops(block=B_load_block)
+    by, bx, ty, tx, bk, ax0, ax1 = sch.get_loops(B_load_block)
     ax0 = sch.fuse(ax0,ax1)
-    # ty, tx, ax0, ax1 = sch.split(loop=ax0,factors=[vty,vtx,None,vload])
-    ax0, ty, tx, ax1 = sch.split(loop=ax0,factors=[None,vty,vtx,vload])
-    # sch.unroll(ax0)
-    sch.bind(ty,"threadIdx.y")
-    sch.bind(tx,"threadIdx.x")
-    sch.vectorize(ax1)
+    ax0, ax1, ax2 = sch.split(loop=ax0,factors=[vty,vtx,None])
+    sch.bind(ax0,"threadIdx.y")
+    sch.bind(ax1,"threadIdx.x")
     
-    by, bx, vy, vx, ty, tx, bk, tk1, tm1, tn1, tk2, tm2, tn2 = sch.get_loops(block=block_Y)
-    sch.unroll(tk1)
-    sch.unroll(tk2)
-    # vunroll = sch.sample_categorical(candidates=[0, 16, 64, 512, 1024], probs=[0.20000000000000001, 0.20000000000000001, 0.20000000000000001, 0.20000000000000001, 0.20000000000000001], decision=3)
-    # sch.annotate(block_or_loop=bk,ann_key="pragma_auto_unroll_max_step",ann_val=vunroll)
+    # A_local_block = sch.cache_read(block_Y,0,"local")
+    
+    # B_local_block = sch.cache_read(block_Y,1,"local")
+    
     
     sch.decompose_reduction(block=block_Y,loop=bk)
     
+    # TODO add padding for some cases
+    # ax0 = sch.get_loops(block=A_local_block)[-2]
+    # ax1 = sch.get_loops(block=A_local_block)[-1]
+    # ax0 = sch.fuse(ax0,ax1)
+    # i , j = sch.split(loop=ax0,factors=[None,4])
+    # ax0 = sch.get_loops(block=B_local_block)[-2]
+    # ax1 = sch.get_loops(block=B_local_block)[-1]
+    # ax0 = sch.fuse(ax0,ax1)
+    # i , j = sch.split(loop=ax0,factors=[None,4])
+    
+    # print(sch.mod.script())
+    
+    by, bx, ty, tx, bk, tk, ax0, ax1= sch.get_loops(block=A_local_block)
+    sch.unroll(ax0)
+    # sch.unroll(ax1)
+    by, bx, ty, tx, bk, tk, ax0, ax1= sch.get_loops(block=B_local_block)
+    sch.unroll(ax0)
+    # sch.unroll(ax1)
+    
+    # TODO add padding for some cases
+    # sch.transform_layout(block=B_local_block,buffer=("read",0),index_map= lambda i,j:( i , ((j//128)*128+(j%128)//8*4+(j%8)//4*64+j%4)))
+    # sch.transform_layout(block=A_local_block,buffer=("read",0),index_map= lambda j,i:( j , ((i//128)*128+(i%128)//8*4+(i%8)//4*64+i%4)))
+
+    # sch.annotate(block_or_loop=A_shared_block,ann_key="meta_schedule.vectorize", ann_val=4)
+    ax0 = sch.get_loops(block=A_shared_block)[-1]
+    sch.vectorize(ax0)
+    ax0 = sch.get_loops(block=B_shared_block)[-1]
+    sch.vectorize(ax0)
+    ax0 = sch.get_loops(block=A_local_block)[-1]
+    sch.vectorize(ax0)
+    ax0 = sch.get_loops(block=B_local_block)[-1]
+    sch.vectorize(ax0)
+    ax0 = sch.get_loops(block=A_load_block)[-1]
+    sch.vectorize(ax0)
+    ax0 = sch.get_loops(block=B_load_block)[-1]
+    sch.vectorize(ax0)
+    ax0 = sch.get_loops(block=sch.get_block(name="Y_local"))[-1]
+    sch.vectorize(ax0)
+    
+    sch.unroll(tk)
+    
     sch.annotate(block_or_loop=A_shared_block,ann_key="double_buffer_scope",ann_val=0)
     sch.annotate(block_or_loop=B_shared_block,ann_key="double_buffer_scope",ann_val=0)
-    by, bx, vy, vx, ty, tx, bk, tk1, tm1, tn1, tk2, tm2, tn2 = sch.get_loops(block=block_Y)
+    by, bx, ty, tx, bk, tk, tm, tn = sch.get_loops(block=block_Y)
     sch.annotate(block_or_loop=bk,ann_key="software_pipeline_stage",ann_val=[0,0,0,0,1])
     sch.annotate(block_or_loop=bk,ann_key="software_pipeline_order",ann_val=[0,3,1,4,2])
-    
-    
     
     return sch
     
